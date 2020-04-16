7bc19744754da48a2441c5f4602ad6d8ad4bae09
代浩然 <daihaoran@qq.com>
2020-04-16 13:15:46 +0800

    增加MFC测试项目

---
+++ MFCMoonPdfTest/MFCMoonPdfTest.h
@@ -0,0 +1,32 @@
+
+// MFCMoonPdfTest.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFCMoonPdfTestApp: 
+// 有关此类的实现，请参阅 MFCMoonPdfTest.cpp
+//
+
+class CMFCMoonPdfTestApp : public CWinApp
+{
+public:
+	CMFCMoonPdfTestApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCMoonPdfTestApp theApp;
